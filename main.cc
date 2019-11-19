3a533ded7812a730ee1109d058a3ca8a68fee366
alexB18 <abrown15@uoregon.edu>
2019-11-19 12:51:18 -0800

    Fixed various syntax errors, enabled the ability to quit the program, and implemented a clear screen command

--- main.cc
+++ main.cc
@@ -1,56 +1,72 @@
 #include <iostream>
 #include <string>
+#include <stdio.h>
+#include <stdlib.h>
 #include "butler.h"
 
 int main(int argc, char const *argv[]) {
 	static bool KILLSWITCH = false;
-	string input = "";
+	std::string input = "";
 
 	Butler butler = Butler();
 
-	std:cout << "Hello and welcome to your virtual butler!" << std:endl;
-	std:cout << "What would you like to name your butler?";
-	std:cin >> input;
+	std::cout << "Hello and welcome to your virtual butler!" << std::endl;
+	std::cout << "What would you like to name your butler?";
+	std::cin >> input;
 
 	// Standard butler template
 	if(input == "Wadsworth"){
-		bulter.setName(wadsworth);
-		std:cout << "Good choice! Wadsworth is our standard (and frankly most basic) Butler" << std:endl;
+		butler.setName("Wadsworth");
+		std::cout << "Good choice! Wadsworth is our standard (and frankly most basic) Butler" << std::endl;
 	}
 
 	// Butler template for the fancier folk
 	else if(input == "Allister"){
-		butler.setName(allister);
-		std:cout << "Aha! I can see one seeks the finer things in life" << std:endl;	
+		butler.setName("Allister");
+		std::cout << "Aha! I can see one seeks the finer things in life" << std::endl;
 	}
 
 	// Coolest butler template
 	else if(input == "Brad"){
-		butler.setName(brad);
-		std:cout << "Sup dude, name's Brad"<< std:endl;
+		butler.setName("Brad");
+		std::cout << "Sup dude, name's Brad"<< std::endl;
 	}
 
 	// Any other name will default to wadsworth
 	else {
-		butler.setName(wadsworth);
-		std:cout << "What a unique name!";
+		butler.setName("Wadsworth");
+		std::cout << "What a unique name!";
 	}
 
 	while(! KILLSWITCH){
 		input = "";
-		std:cout << "For a list of commands, enter \'c\'. Else, enter \'q\' to quit" << std:endl;
-		std:cout << ">> ";
-		std:cin >> input;
+		std::cout << "For a list of commands, enter \'c\'. Else, enter \'q\' to quit" << std::endl;
+		std::cout << ">> ";
+		std::cin >> input;
 
 		if(input == "c" || input == "C"){
-			std:cout << "Generic Commands (Static Regardless of Butler Type):" << std:endl;
-			std:cout << "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~" <<std:endl;
+			std::cout << "Generic Commands (Static Regardless of Butler Type):" << std::endl;
+			std::cout << "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~" << std::endl;
 
-			std:cout << "weather -> opens weather app using default browser"
-			std:cout << ""
+			std::cout << "weather -> opens weather app using default browser" << std::endl;
+			std::cout << "clear -> clears terminal window";
+		}
+
+		if(input == "q" || input == "Q"){
+			std::cout << "Goodbye!" << std::endl;
+			KILLSWITCH = true;
+			break;
+		}
+
+		if(input == "clear"){
+			system("scripts/clearscreen.sh");
 		}
 	}
-	std::cout << "Hello! " << std::endl;
-	std::cout << "My name is " << butler.getName() << std::endl;
+
+	if( ! KILLSWITCH){
+		std::cout << "Hello! " << std::endl;
+		std::cout << "My name is " << butler.getName() << std::endl;
+	}
+
 
 }
